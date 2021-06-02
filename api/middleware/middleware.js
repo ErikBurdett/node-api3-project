ee81eae044a2cedd0cb34b8026795654420816a7
erik-burdett <erikaburdet@gmail.com>
2021-06-02 16:13:24 -0500

    finished but can't seem to get tests to pass - doubled checked with guided solution video and still can't get passing, may be from reseting lambda.db3?

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -14,9 +14,7 @@ async function validateUserId(req, res, next) {
   try{
     const user = await User.getById(req.params.id)
     if(!user){
-      res.status(404).json({
-        message: 'no such user',
-      })
+     next({status: 404, message: 'user not found'})
     }else{
       req.user = user 
       next()
@@ -24,7 +22,7 @@ async function validateUserId(req, res, next) {
 
   }catch (err){
     res.status(500).json({
-      message: 'Error finding user',
+      message: 'problem finding user',
   })
 }
 }
